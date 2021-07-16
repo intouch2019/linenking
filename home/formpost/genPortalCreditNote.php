60e6a305de8db4663590a6ff414d3c1454d38b3b
rghuleonintouch <65955922+rghuleonintouch@users.noreply.github.com>
2021-07-16 12:01:02 +0530

    Update genPortalCreditNote.php

--- home/formpost/genPortalCreditNote.php
+++ home/formpost/genPortalCreditNote.php
@@ -18,13 +18,24 @@ if(isset($store) && $store !="" && isset($user_id) && $user_id !="")
    $item_exist=$db->fetchObject("select id from it_portalinv_items_creditnote where is_proccessed=0 and store_id=$store");
     if(isset($item_exist) && ! empty($item_exist) && $item_exist != null)
      {
-        $credit_no="DCN-";
+        
+        $credit_no="CNLK-";
         $credit_num=0;
-       $creditno=$db->fetchObject("select cn_no from dgcreditnote_no"); 
-       if($creditno)
+       $creditno=$db->fetchObject("select cn_no from creditnote_no where active=0"); 
+      
+       if(isset($creditno) && ! empty($creditno) && $creditno != null)
        {
-           $credit_no .=$creditno->cn_no+1;
-           $credit_num=$creditno->cn_no+1;}
+           $credit_no .=$creditno->cn_no;
+           $db->execUpdate("update creditnote_no set active=1");
+           $credit_num=$creditno->cn_no+1;
+        
+//        $credit_no="DCN-";
+//        $credit_num=0;
+//       $creditno=$db->fetchObject("select cn_no from dgcreditnote_no"); 
+//       if($creditno)
+//       {
+//           $credit_no .=$creditno->cn_no+1;
+//           $credit_num=$creditno->cn_no+1;}
            $qty=0;
             $mrp_total=0.0;
             $disc_total=0.0;
@@ -62,47 +73,54 @@ if(isset($store) && $store !="" && isset($user_id) && $user_id !="")
         $update_query="update it_portalinv_items_creditnote set  is_proccessed=1 ,invoice_id=$insert_id where is_proccessed=0 and store_id=$store";
         $db->execUpdate($update_query);
                 if($insert_id){
-        $db->execUpdate("update dgcreditnote_no set cn_no=$credit_num");
+                    $db->execUpdate("update creditnote_no set cn_no=$credit_num");  
+//        $db->execUpdate("update dgcreditnote_no set cn_no=$credit_num");
         
-        $records = $credit_num . "<>1";
+//        $records = $credit_num . "<>1";
 //        $db = new DBConn();
 //        $url = "http://localhost/cottonking/home/sendCN/sendDefCNnumber.php";
        //   $url = "http://192.168.0.38/ck_new_y/home/sendCN/sendDefCNnumber.php";
        
           
-        $url = "http://cottonking.intouchrewards.com/sendCN/sendDefCNnumber.php";
-          
-        $fields = array('records' => urlencode($records));
-        $fields_string = "";
-        foreach ($fields as $key => $value) {
-            $fields_string .= $key . '=' . $value . '&';
-        }
-        rtrim($fields_string, '&');
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_POST, true);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_POST, count($fields));
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $fields_string);
-
-        $outputresult = curl_exec($ch);
-        $info = curl_getinfo($ch);
-        curl_close($ch);
+//        $url = "http://cottonking.intouchrewards.com/sendCN/sendDefCNnumber.php";
+//          
+//        $fields = array('records' => urlencode($records));
+//        $fields_string = "";
+//        foreach ($fields as $key => $value) {
+//            $fields_string .= $key . '=' . $value . '&';
+//        }
+//        rtrim($fields_string, '&');
+//        $ch = curl_init();
+//        curl_setopt($ch, CURLOPT_POST, true);
+//        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+//        curl_setopt($ch, CURLOPT_URL, $url);
+//        curl_setopt($ch, CURLOPT_POST, count($fields));
+//        curl_setopt($ch, CURLOPT_POSTFIELDS, $fields_string);
+//
+//        $outputresult = curl_exec($ch);
+//        $info = curl_getinfo($ch);
+//        curl_close($ch);
                 }
         
         $_SESSION['form_success'] ="Credit note '$credit_no' crated for $storename";
+           $db->execUpdate("update creditnote_no set active=0"); 
+     }  else {
+           $errors['CnNum_error'] = "Credit Note Number used in another credit note,so please try after some time"; 
+       }
         
      
     }
     else{
-       $_SESSION['form_errors'] = "Items not available for store id $store"; 
+       $errors['form_errors'] = "Items not available for store id $store"; 
     }
 }else{
- $_SESSION['form_errors'] = "Error during creatinp defective garments creditnote"; 
+ $errors['form_errors'] = "Error during creatinp defective garments creditnote"; 
 }
 } catch (Exception $ex) {}
  
-
+if (count($errors) > 0) {
+	$_SESSION['form_errors'] = $errors;
+}
 session_write_close();
 header("Location: ".DEF_SITEURL."create/creditnote");
 exit;
