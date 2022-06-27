876f9352a718ac4258136a82a42453df3c87dcf5
ankushbhoir <abhoir@intouchrewards.com>
2022-06-27 17:35:52 +0530

    add store audit form

---
+++ home/view/cls_addstoreaudit.php
@@ -0,0 +1,250 @@
+<?php
+require_once "view/cls_renderer.php";
+require_once "lib/db/DBConn.php";
+require_once "lib/core/Constants.php";
+require_once "session_check.php";
+require_once "lib/core/strutil.php";
+
+class cls_addstoreaudit extends cls_renderer {
+
+    var $currStore;
+    var $params;
+    var $storeid;
+    var $dtrange;
+    var $sid;
+    var $aid;
+    var $view;
+     
+    function __construct($params = null) {
+        $this->currStore = getCurrUser();
+        $this->params = $params;
+        $this->dtrange = date("d-m-Y");
+        if (isset($params["sid"]) && $params["sid"] != "") {
+            $this->sid = $params["sid"];
+        }
+        if (isset($params["aid"]) && $params["aid"] != "") {
+            $this->aid = $params["aid"];
+        }
+        if (isset($params["view"]) && $params["view"] != "") {
+            $this->view = $params["view"];
+        }
+    }
+
+    function extraHeaders() {
+        if (!$this->currStore) {
+            ?>
+            <h2>Session Expired</h2>
+            Your session has expired. Click <a href="">here</a> to login.
+            <?php
+            return;
+        }
+        ?>
+        <script type="text/javascript" src="js/expand.js"></script>
+        <link rel="stylesheet" href="css/prettyPhoto.css" type="text/css" media="screen" title="prettyPhoto main stylesheet" charset="utf-8" />
+        <script src="js/prettyPhoto/jquery.prettyPhoto.js" type="text/javascript" charset="utf-8"></script>
+       
+
+       
+        </script>
+<script>
+
+    function getselecteddate(){
+     auditdate=document.getElementById("AuditDate").value;
+     auditdate2=document.getElementById("AuditDate2").value;
+     if(auditdate==auditdate2){
+         document.getElementById("submit1").disabled=true;
+//         document.getElementById("statusmsg").innerHTML="*Please check Audit Date".fontcolor("red");
+         
+     }else{
+         document.getElementById("submit1").disabled=false;
+//         document.getElementById("statusmsg").hidden=true;
+     }
+    
+}
+
+
+function validationform(){
+    var error=0;
+    var managername=document.getElementById("Manager_name").value;
+    var Managerphone=document.getElementById("Managerphone").value;
+    var auditorname=document.getElementById("Auditor_name").value;
+    var auditdate=document.getElementById("AuditDate").value;
+    var remark=document.getElementById("remark").value;
+//    alert(managername);
+    if(managername==null || managername==""){
+      alert("Field Manager_name Could Not be Blank");
+      error++;
+    } else if(Managerphone==null || Managerphone==""){
+        alert("Field Managerphone Could Not be Blank"); 
+        error++;
+    }else if(auditorname==null || auditorname==""){
+        alert("Field Auditor_name Could Not be Blank");
+        error++;
+    }else if(auditdate==null || auditdate==""){
+        alert("Field AuditDate Could Not be Blank");
+        error++;
+    }else if(remark==null || remark==""){
+       alert("Field remark Could Not be Blank");  
+       error++;
+    }
+      totques=document.getElementById("totalquestions").value;
+    for(i=1;i<=totques;i++){
+        ansquestionyes=document.getElementById("queyes"+i).checked;
+        ansquestionno=document.getElementById("queno"+i).checked;
+       // alert("<b>"+ansquestionyes+"<b>"+i+ansquestionno);
+        if(ansquestionyes == false){
+            if(ansquestionno == false){
+            alert("Please select the options for all questions");
+            error++;
+            break;
+        }
+        }
+        }
+
+    
+    var phoneno = /^\d{10}$/;
+    //alert(Managerphone.match(phoneno));
+  if(!Managerphone.match(phoneno))
+  {
+        alert("incorrect Mobile Number");
+        error++;
+       document.getElementById("Managerphone").value="";
+        }
+      //alert(error);  
+    if(error==0){
+        var aconfirm = confirm("Are You Sure to submit the form ?");
+        if(aconfirm == 1){
+            var a=0;
+             document.getElementById("myForm").submit();
+        }else{
+           document.getElementById("auditdate").value="";
+        } 
+    }
+    
+    
+}
+
+
+</script>
+        <?php
+    }
+
+
+    public function pageContent() {
+        $menuitem = "addstoreaudit";
+        $db = new DBConn();
+        include "sidemenu." . $this->currStore->usertype . ".php";
+
+        ?>
+
+        <div class="grid_10">
+                <div class="box">
+
+                    <?php 
+                 
+                    $audit =$db->fetchObject("select * from it_auditdetails where id =$this->aid order by id desc ");
+                   
+                 ?> 
+                  
+
+                    <h2>
+                        <a href="#" id="toggle-accordion" style="cursor: pointer; ">Store Audit Form</a>
+                    </h2><br>
+                    <div style="margin-top: 0px; margin-right: 0px; margin-bottom: 0px; margin-left: 0px; position: static; overflow-x: hidden; overflow-y: hidden; ">
+                        <form action ="formpost/storeaudit.php" id="myForm" method="POST">
+                        <div class="block" id="accordion" style="margin-top: 0px; margin-right: 0px; margin-bottom: 0px; margin-left: 0px; ">
+                            <div id="accordion">
+                           
+
+                                    <div class="grid_12">
+                                        <div class="grid_4">
+                                        <label><span style="color:red">*</span>Manager Name: </label>
+                                        <input type="text"  id="Manager_name" name="Manager_name" style="width:100%;height:23px;font-size:14px;" required value="<?php if(isset($audit)){echo $audit->Manager_name;} ?>"required>
+                                        </div>
+                                       
+                                        <div class="grid_4">
+                                        <label><span style="color:red">*</span>Manager Mobile Number : </label>
+                                        <input type="text" id="Managerphone" name="Managerphone"  style="width:100%;height:23px;font-size:14px;" required value="<?php if(isset($audit)){echo $audit->Managerphone;} ?>"required>
+                                        </div>
+                                    </div>
+                              
+
+
+
+                                    <div class="grid_12">
+                                        <br>
+                                        <div class="grid_4">
+                                        <label><span style="color:red">*</span>Auditor Name: </label>
+                                        <input type="text" id="Auditor_name" name="Auditor_name" style="width:100%;height:23px;font-size:14px;" required value="<?php if(isset($audit)){echo $audit->Auditor_name;} ?>"required>
+                                        </div>
+                                        
+                                        <div class="grid_2">
+                                    <lable style="font-size:10px;"><span style="color:red">*</span>Audit Date:</lable>
+                                    
+                                    <input type="date" id="AuditDate" name="AuditDate" min="<?= date("Y-m-d",strtotime($audit->AuditDate.'+1 day'))?>" max="<?= date('Y-m-d'); ?>" style="width:100%;height:23px;font-size:14px;" value="<?= date("Y-m-d",strtotime($audit->AuditDate))?>" onchange ="getselecteddate()" required>&nbsp;&nbsp;&nbsp;&nbsp;
+                                    <input type="date" hidden id="AuditDate2" name="AuditDate2" min="<?= date("Y-m-d",strtotime($audit->AuditDate.'+1 day'))?>" max="<?= date('Y-m-d'); ?>" style="width:100%;height:23px;font-size:14px;" value="<?= date("Y-m-d",strtotime($audit->AuditDate))?>" onchange ="getselecteddate()" required>&nbsp;&nbsp;&nbsp;&nbsp;
+                                    
+                                        </div>
+                                    </div>
+                                  </div>
+                                  </div>
+                
+                            <div class="box" style="margin-left:0px; overflow:auto; height:350px; padding-bottom: 20px">
+                            <table> 
+                                    <tr>
+                                        <th>ID</th>
+                                        <th> Description</th>
+                                        <th> Yes</th>
+                                        <th> No</th>
+
+
+                                    </tr>
+                                    <?php
+                                  
+                                    $rules = $db->fetchObjectArray("select * from it_auditquestions order by id");?>
+                                    <input type="hidden" id="totalquestions"  value=<?php echo sizeof($rules);?>><?php
+
+                                    foreach ($rules as $rule) {
+                                        if(isset($this->aid) && $this->aid !==""){
+                                        $audit_response =$db->fetchObject("select is_opted from it_auditresponse where audit_id =$this->aid  and question_id= $rule->ID");
+                                       
+                                    }?>
+                                        <tr>
+                                            <td><?php echo $rule->ID; ?></td>
+                                            <td><?php echo $rule->description; ?></td>
+                                            <td style="width:8%"><input type ="radio" name="que<?php echo $rule->ID?>" id="queyes<?php echo $rule->ID?>" style="width:20%" <?php if (isset ($audit_response) && $audit_response->is_opted == 1) { ?>checked <?php } ?> value="1" required>Yes</td>
+                                            <td style="width:8%"><input type ="radio" name="que<?php echo $rule->ID?>" id="queno<?php echo $rule->ID?>" style="width:20%" <?php if (isset ($audit_response) && $audit_response->is_opted == 0) { ?>checked <?php } ?> value="0" required>No</td>
+                                        </tr>
+                                    <?php }?>
+                                        <tr></tr>
+                                </table>
+                                <div class="grid_12">
+                                    <label id="t_address"><b> <span style="color:red">*</span> Observations & Remarks:</b> </label><br>
+                                    <!--<input type="text" name="remark" style="width:100%" style="length:100%" required value="<?php if(isset($audit)){echo $audit->remark;} ?>"required>-->
+                                    <textarea id="remark" name="remark" required value="" rows="2" cols="100"><?php if(isset($audit)){echo $audit->remark;} ?></textarea>
+                                    <br><br>
+                                </div>
+                                
+                                <input type="hidden" value="<?php echo $this->sid;?>"   name="store_id" id="store_id">
+
+                          
+                            <?php if ($this->view!=1 ){?>
+                            <div align='center'>
+                                
+                                <input type="submit" id="submit1" onclick="validationform()" value="Submit Audit Form">
+<!--                                <br><span id="statusmsg"></span>-->
+                            </div>
+                            <?php }?>
+                            </div>
+                       
+              </form>
+                                     </div> 
+                </div>  
+            <script>getselecteddate();</script> 
+        </div>
+        <?php
+    }
+
+}
+?>
+  
---
+++ home/view/cls_store_audit.php
@@ -0,0 +1,221 @@
+
+<?php
+require_once "view/cls_renderer.php";
+require_once ("lib/db/DBConn.php");
+require_once ("lib/core/Constants.php");
+
+
+class cls_store_audit extends cls_renderer {
+
+    var $currUser;
+    var $userid;
+    var $dtrange;
+    var $sid;
+    var $currStore;
+
+    function __construct($params = null) {
+        //parent::__construct(array(UserType::Admin, UserType::CKAdmin));
+        $this->currUser = getCurrUser();
+        $this->userid = $this->currUser->id;
+        $this->currStore = getCurrUser();
+        $this->params = $params;
+        if (!$this->currStore) {
+            return;
+        }
+        if ($params && isset($params['sid'])) {
+            $this->sid = $params['sid'];
+        }
+    }
+
+    function extraHeaders() {
+        ?>
+        <link rel="stylesheet" href="jqueryui/css/custom-theme/jquery-ui-1.8.14.custom.css" type="text/css" media="screen" charset="utf-8" />
+        <script type="text/javascript" src="js/expand.js"></script>
+        <script language="JavaScript" src="js/tigra/validator.js"></script>
+        <script type="text/javascript">
+            /************************************************************************************************************
+             (C) www.dhtmlgoodies.com, April 2006
+             
+             This is a script from www.dhtmlgoodies.com. You will find this and a lot of other scripts at our website.	
+             
+             Terms of use:
+             You are free to use this script as long as the copyright message is kept intact. However, you may not
+             redistribute, sell or repost it without our permission.
+             
+             Thank you!
+             
+             www.dhtmlgoodies.com
+             Alf Magne Kalleland
+             
+             ************************************************************************************************************/
+
+        </script>
+        <link rel="stylesheet" href="css/bigbox.css" type="text/css" />
+        <script type="text/javascript" src="jqueryui/js/jquery-ui-1.7.1.custom.min.js"></script>
+        <script type="text/javascript" src="js/daterangepicker.jQuery.js"></script>
+        <link rel="stylesheet" href="css/ui.daterangepicker.css" type="text/css" />
+        <link rel="stylesheet" href="css/redmond/jquery-ui-1.7.1.custom.css" type="text/css" title="ui-theme" />
+        <script type="text/javascript">
+
+            function addstoreaudit1() {
+               
+                var getstoreid = document.getElementById("avalue").value;
+               // alert(getstoreid);
+                window.location.href = "addstoreaudit/sid=" + getstoreid;
+                //  window.location.href = "designs/search/dno=" + design_no;
+            }
+            
+             function getstoredetails() {
+                
+                var getstoreid = document.getElementById("avalue").value;
+               // alert(getstoreid);
+                window.location.href = "store/audit/sid=" + getstoreid;
+                //  window.location.href = "designs/search/dno=" + design_no;
+            }
+          
+        </script>
+<style>
+.tooltip {
+  position: relative;
+  display: inline-block;
+  border-bottom: 1px dotted black;
+}
+
+.tooltip .tooltiptext {
+  border:1px solid black;
+  visibility: hidden;
+  width: 200px;
+  background-color: white;
+  color:black;
+  text-align: left;
+  border-radius: 2px;
+  padding: 5px;
+  bottom: 100%;
+  left: 50%;
+ margin-left: 0px;
+
+  /* Position the tooltip */
+  position: absolute;
+  z-index: 1;
+}
+
+.tooltip:hover .tooltiptext {
+  visibility: visible;
+}
+</style>
+        <?php
+    }
+
+    public function pageContent() {
+        $currUser = getCurrUser();
+        $menuitem = "store_audit";
+        include "sidemenu." . $currUser->usertype . ".php";
+        $formResult = $this->getFormResult();
+        $db = new DBConn();
+        $sids = explode(',', $this->sid);
+        ?>
+        <div class="grid_10">
+       
+            <fieldset>                         
+              
+                <legend>Stores</legend>
+              
+                        <div class="grid_2">
+                            <lable style="font-size:15px;">Select Store</lable>
+                        </div>   
+
+                        <div class="grid_4">
+                            <select name="avalue" id="avalue" data-placeholder="Select Store" class="chzn-select" single style="width:100%;" onchange= getstoredetails()>
+
+                                <option value="1" selected>Select Store</option>
+
+
+                                <?php
+                                $objs = $db->fetchObjectArray("select id,store_name from it_codes where usertype=" . UserType::Dealer . " and inactive=0 and isastore=1 and is_closed=0 order by store_name");
+
+                                if ($objs) {
+                                    foreach ($objs as $obj) {
+
+
+                                    if (in_array($obj->id, $sids)){
+                                    
+                                        $sel = 'selected';
+                                        
+                                    } else {
+                                        $sel = '';
+                                    }
+                                        ?>
+
+                                        <option value="<?php echo $obj->id; ?>"<?php echo $sel; ?>  ><?php echo $obj->store_name; ?></option>
+                                        <?php
+                                    }
+                                }
+                                ?>
+
+                                <?php ?>
+                            </select>
+                            </br>
+
+                        </div>
+               
+                        
+                        &nbsp;&nbsp;&nbsp; &nbsp; &nbsp; &nbsp;   
+                        <button><a href="/linenking/home/tmp1/StoreAuditform.pdf" download >Download Store Audit Form</a></button>
+                       <br><br> <div><table border="3">
+                               
+                    <tr>
+                        <th colspan="15"><h4>Store Audit Details</h4></th>
+                    </tr>
+                    <tr>
+                        <th>Audit id</th>
+                        <th>Store Name</th>
+                        <th>Manager Name</th>
+                        <th>Manager Mobile Number</th>
+                        <th>Auditor Name</th>
+                        <th>Audit Date</th>
+                        <th>Submitted Date</th>
+                        <th>Remarks</th>
+                         <th>Action</th>
+                    </tr>
+                    
+ 
+                       <?php
+                       if (isset($this->sid)&& $this->sid!==""){
+                           $qsid="and a.store_id=$this->sid"; 
+                       }else{
+                           $qsid="and a.store_id=-1";
+                       }
+                                $objsdetails = $db->fetchObjectArray("select a.*,s.store_name from it_auditdetails a, it_codes s  where a.store_id= s.id $qsid order by a.id desc ");
+                               
+                                $max=0; $i= sizeof($objsdetails);
+                                if($i==0 && $this->sid > 1){ ?><h6 span class="error" style="color:white;"> No Store Audit Report Available For Selected Store </h6></span><br><div align='right'class="grid_4"><button onclick = "addstoreaudit1()">Create Store Audit Report</button><br><br></div><?php }
+                            foreach ($objsdetails as $obj) {    
+                        ?>
+                                
+                        <tr>
+                                            <td><?php echo $i ?></td>
+                                            <td><?php echo $obj->store_name; ?></td>
+                                            <td><?php echo $obj->Manager_name; ?></td>
+                                            <td><?php echo $obj->Managerphone; ?></td>
+                                            <td><?php echo $obj->Auditor_name; ?></td>
+                                            <td><?php echo $obj->AuditDate; ?></td>
+                                            <td><?php echo $obj->SubmittedDate; ?></td>
+                                            <td><div class="tooltip">Show Remark<span class="tooltiptext"><?php echo $obj->remark; ?></span></div></td> 
+                                                  
+                                                                                                  
+                                            <?php if($max==0){?><td><a href='addstoreaudit/aid=<?php echo $obj->id;?>/sid=<?php echo $obj->store_id;?>'>View & Edit</a></td> <?php $max++; }
+                                             elseif($max > 0 && $max <=2){?><td><a href='addstoreaudit/aid=<?php echo $obj->id;?>/sid=<?php echo $obj->store_id;?>/view=<?php echo 1;?>'>View</a></td> <?php $max++; }else{?> <td></td> <?php }?>
+                                            
+                                            
+                                            
+                        </tr>
+                        <?php $i--; }?></table></div>
+</fieldset> </div>
+        
+
+                        <?php
+                      
+                    }
+
+                }
+                ?>
