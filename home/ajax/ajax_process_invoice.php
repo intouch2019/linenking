47e4de3cb0f863ae29cf1b38111bb4e28bc5b425
Nikhil-Morusakar <nmoruskar@intouchrewards.com>
2024-10-29 12:54:57 +0530

    Creating intransit clearing page

---
+++ home/ajax/ajax_process_invoice.php
@@ -0,0 +1,20 @@
+<?php
+
+require_once "../../it_config.php";
+require_once "lib/db/DBConn.php";
+require_once "lib/orders/clsOrders.php";
+require_once ("session_check.php");
+require_once "lib/logger/clsLogger.php";
+    extract($_GET);
+     $id = ($_GET['id']);
+        $db = new DBConn();
+
+$query = "update it_invoices set is_procsdForRetail = 1 where id = '$id'";
+$obj = $db->execUpdate($query);
+
+if($obj == "1"){
+    echo "1";
+} else {
+    echo "0"; // return error if update fails
+}
+?>
