7b77c8d181cad1f73bd70e5e101831294b16af52
Nikhil-Morusakar <nmoruskar@intouchrewards.com>
2025-09-20 17:43:35 +0530

    MRP wise store stock current report changes - excluded some categories

--- home/formpost/storecurrstockMrpwiseExcel.php
+++ home/formpost/storecurrstockMrpwiseExcel.php
@@ -23,10 +23,10 @@ $objPHPExcel->getActiveSheet()->setCellValue('A1', 'Sr No');
 $objPHPExcel->getActiveSheet()->setCellValue('B1', 'Store Name');
 $objPHPExcel->getActiveSheet()->setCellValue('C1', 'Total Qty (MRP <=1050)');
 $objPHPExcel->getActiveSheet()->setCellValue('D1', 'Total Value (MRP <=1050)');
-$objPHPExcel->getActiveSheet()->setCellValue('E1', 'Total Qty (MRP 1051-2625)');
-$objPHPExcel->getActiveSheet()->setCellValue('F1', 'Total Value (MRP 1051-2625)');    
-$objPHPExcel->getActiveSheet()->setCellValue('G1', 'Total Qty (MRP >2625)');
-$objPHPExcel->getActiveSheet()->setCellValue('H1', 'Total Value (MRP >2625)');
+$objPHPExcel->getActiveSheet()->setCellValue('E1', 'Total Qty (MRP 1051-2800)');
+$objPHPExcel->getActiveSheet()->setCellValue('F1', 'Total Value (MRP 1051-2800)');    
+$objPHPExcel->getActiveSheet()->setCellValue('G1', 'Total Qty (MRP >2800)');
+$objPHPExcel->getActiveSheet()->setCellValue('H1', 'Total Value (MRP >2800)');
 $objPHPExcel->getActiveSheet()->setCellValue('I1', 'Total Qty');
 $objPHPExcel->getActiveSheet()->setCellValue('J1', 'Total Value');
 $objPHPExcel->getActiveSheet()->setCellValue('K1', 'Debit Note Value');
@@ -109,18 +109,18 @@ $srno = 1;
 if ($sid == -1) {
     $filenameas_storename = "AllStore_";
     $iquery = "select c.store_name,SUM(cs.quantity) AS total_quantity,SUM(i.MRP * cs.quantity) AS total_value,SUM(CASE WHEN i.MRP <= 1050 THEN cs.quantity ELSE 0 END) AS qty_upto_1050,"
-            . " SUM(CASE WHEN i.MRP <= 1050 THEN i.MRP * cs.quantity ELSE 0 END) AS val_upto_1050,SUM(CASE WHEN i.MRP BETWEEN 1051 AND 2625 THEN cs.quantity ELSE 0 END) AS qty_1051_2625,"
-            . " SUM(CASE WHEN i.MRP BETWEEN 1051 AND 2625 THEN i.MRP * cs.quantity ELSE 0 END) AS val_1051_2625,SUM(CASE WHEN i.MRP > 2625 THEN cs.quantity ELSE 0 END) AS qty_above_2625,"
-            . " SUM(CASE WHEN i.MRP > 2625 THEN i.MRP * cs.quantity ELSE 0 END) AS val_above_2625 from it_codes c,it_current_stock cs,"
+            . " SUM(CASE WHEN i.MRP <= 1050 THEN i.MRP * cs.quantity ELSE 0 END) AS val_upto_1050,SUM(CASE WHEN i.MRP BETWEEN 1051 AND 2800 THEN cs.quantity ELSE 0 END) AS qty_1051_2800,"
+            . " SUM(CASE WHEN i.MRP BETWEEN 1051 AND 2800 THEN i.MRP * cs.quantity ELSE 0 END) AS val_1051_2800,SUM(CASE WHEN i.MRP > 2800 THEN cs.quantity ELSE 0 END) AS qty_above_2800,"
+            . " SUM(CASE WHEN i.MRP > 2800 THEN i.MRP * cs.quantity ELSE 0 END) AS val_above_2800 from it_codes c,it_current_stock cs,"
             . " it_items i, it_categories ctg, it_brands br, it_styles st, it_sizes si, it_fabric_types fb, it_materials mt, it_prod_types pr,"
             . " it_mfg_by mfg where c.id = cs.store_id and c.usertype=".UserType::Dealer." and c.is_closed = 0 and cs.barcode = i.barcode and ctg.id=i.ctg_id and br.id=i.brand_id"
             . " and st.id=i.style_id and si.id=i.size_id and pr.id=i.prod_type_id and mt.id=i.material_id and fb.id=i.fabric_type_id and mfg.id=i.mfg_id"
             . " and cs.store_id = c.id and cs.quantity !=0 and ctg.id not in (65,16,13,14,18,19,20,24,25,26,27,28,29,33,35,41,42,43,44,45,46,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64) group by c.store_name"; 
 } else {
     $iquery = "select c.store_name,SUM(cs.quantity) AS total_quantity,SUM(i.MRP * cs.quantity) AS total_value,SUM(CASE WHEN i.MRP <= 1050 THEN cs.quantity ELSE 0 END) AS qty_upto_1050,"
-            . " SUM(CASE WHEN i.MRP <= 1050 THEN i.MRP * cs.quantity ELSE 0 END) AS val_upto_1050,SUM(CASE WHEN i.MRP BETWEEN 1051 AND 2625 THEN cs.quantity ELSE 0 END) AS qty_1051_2625,"
-            . " SUM(CASE WHEN i.MRP BETWEEN 1051 AND 2625 THEN i.MRP * cs.quantity ELSE 0 END) AS val_1051_2625,SUM(CASE WHEN i.MRP > 2625 THEN cs.quantity ELSE 0 END) AS qty_above_2625,"
-            . " SUM(CASE WHEN i.MRP > 2625 THEN i.MRP * cs.quantity ELSE 0 END) AS val_above_2625 from it_codes c,it_current_stock cs,"
+            . " SUM(CASE WHEN i.MRP <= 1050 THEN i.MRP * cs.quantity ELSE 0 END) AS val_upto_1050,SUM(CASE WHEN i.MRP BETWEEN 1051 AND 2800 THEN cs.quantity ELSE 0 END) AS qty_1051_2800,"
+            . " SUM(CASE WHEN i.MRP BETWEEN 1051 AND 2800 THEN i.MRP * cs.quantity ELSE 0 END) AS val_1051_2800,SUM(CASE WHEN i.MRP > 2800 THEN cs.quantity ELSE 0 END) AS qty_above_2800,"
+            . " SUM(CASE WHEN i.MRP > 2800 THEN i.MRP * cs.quantity ELSE 0 END) AS val_above_2800 from it_codes c,it_current_stock cs,"
             . " it_items i, it_categories ctg, it_brands br, it_styles st, it_sizes si, it_fabric_types fb, it_materials mt, it_prod_types pr,"
             . " it_mfg_by mfg where c.id = cs.store_id and cs.store_id=$sid and cs.barcode = i.barcode and ctg.id=i.ctg_id and br.id=i.brand_id"
             . " and st.id=i.style_id and si.id=i.size_id and pr.id=i.prod_type_id and mt.id=i.material_id and fb.id=i.fabric_type_id and mfg.id=i.mfg_id"
@@ -137,14 +137,14 @@ if (!empty($alldealersobj) && isset($alldealersobj)) {
         $objPHPExcel->getActiveSheet()->setCellValueByColumnAndRow(1, $rowCount, $obj->store_name);
         $objPHPExcel->getActiveSheet()->setCellValueByColumnAndRow(2, $rowCount, $obj->qty_upto_1050);
         $objPHPExcel->getActiveSheet()->setCellValueByColumnAndRow(3, $rowCount, $obj->val_upto_1050);
-        $objPHPExcel->getActiveSheet()->setCellValueByColumnAndRow(4, $rowCount, $obj->qty_1051_2625);
-        $objPHPExcel->getActiveSheet()->setCellValueByColumnAndRow(5, $rowCount, $obj->val_1051_2625);
-        $objPHPExcel->getActiveSheet()->setCellValueByColumnAndRow(6, $rowCount, $obj->qty_above_2625);
-        $objPHPExcel->getActiveSheet()->setCellValueByColumnAndRow(7, $rowCount, $obj->val_above_2625);
+        $objPHPExcel->getActiveSheet()->setCellValueByColumnAndRow(4, $rowCount, $obj->qty_1051_2800);
+        $objPHPExcel->getActiveSheet()->setCellValueByColumnAndRow(5, $rowCount, $obj->val_1051_2800);
+        $objPHPExcel->getActiveSheet()->setCellValueByColumnAndRow(6, $rowCount, $obj->qty_above_2800);
+        $objPHPExcel->getActiveSheet()->setCellValueByColumnAndRow(7, $rowCount, $obj->val_above_2800);
         $objPHPExcel->getActiveSheet()->setCellValueByColumnAndRow(8, $rowCount, $obj->total_quantity);
         $objPHPExcel->getActiveSheet()->setCellValueByColumnAndRow(9, $rowCount, $obj->total_value);
-        $objPHPExcel->getActiveSheet()->setCellValueByColumnAndRow(10, $rowCount, round($obj->val_1051_2625 - (($obj->val_1051_2625/1.12)*1.05)));
-        $objPHPExcel->getActiveSheet()->setCellValueByColumnAndRow(11, $rowCount, round($obj->val_above_2625 - (($obj->val_above_2625/1.12)*1.18)));
+        $objPHPExcel->getActiveSheet()->setCellValueByColumnAndRow(10, $rowCount, round($obj->val_1051_2800 - (($obj->val_1051_2800/1.12)*1.05)));
+        $objPHPExcel->getActiveSheet()->setCellValueByColumnAndRow(11, $rowCount, round($obj->val_above_2800 - (($obj->val_above_2800/1.12)*1.18)));
         
         $rowCount++;
         $srno++;
